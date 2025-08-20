dc6aa450fcd5f08659221c47a3f13542fcb8a8b8
Leon König <koenig.leon2000@gmail.com>
2025-08-20 14:24:42 +0200

    base structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
-  backgroundGradient = ["#0B042D", "#1F0B80", "#0B042F", "#241085"];
 }
---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { WhyMeComponent } from './why-me/why-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
+
+@Component({
+  selector: 'app-main-content',
+  standalone: true,
+  imports: [
+    LandingPageComponent, WhyMeComponent, SkillsComponent, 
+    ProjectsComponent, ReferencesComponent, ContactComponent],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss'
+})
+export class MainContentComponent {
+
+}
