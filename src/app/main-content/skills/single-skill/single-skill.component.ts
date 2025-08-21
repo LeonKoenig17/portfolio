894df079b6d10865ca82b2873b06da5db3ffd4b2
Leon König <koenig.leon2000@gmail.com>
2025-08-21 12:00:21 +0200

    skill component init

---
+++ src/app/main-content/skills/single-skill/single-skill.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-skill',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-skill.component.html',
+  styleUrl: './single-skill.component.scss'
+})
+export class SingleSkillComponent {
+  
+}
--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { SingleSkillComponent } from './single-skill/single-skill.component';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [SingleSkillComponent],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  skills = [
+    {img: "./assets/images/angular.png", text: "Angular"},
+    {img: "./assets/images/typescript.png", text: "Typescript"},
+    {img: "./assets/images/javascript.png", text: "JavaScript"},
+    {img: "./assets/images/html.png", text: "HTML"},
+    {img: "./assets/images/css.png", text: "CSS"},
+    {img: "./assets/images/rest-api.png", text: "REST-API"},
+    {img: "./assets/images/firebase.png", text: "Firebase"},
+    {img: "./assets/images/git.png", text: "Git"},
+    {img: "./assets/images/scrum.png", text: "Scrum"},
+    {img: "./assets/images/material-design.png", text: "Material design"},
+    {img: "./assets/images/challenge-me.png", text: "Challenge me"}
+  ]
 }
