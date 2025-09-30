d6ac912e7f51dbce926806646d0fa5d5ee9bd127
Leon König <koenig.leon2000@gmail.com>
2025-09-30 11:44:04 +0200

    mobile context menu complete

--- src/app/main-content/landing-page/landing-page.component.ts
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ScrollService } from '../../shared/services/scroll.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
@@ -14,4 +15,6 @@ export class LandingPageComponent {
   scrollTo(sectionId: string) {
     this.scrollService.scrollTo(sectionId);
   }
+
+  isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent) || window.innerWidth < 768;
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ScrollService } from '../../services/scroll.service';
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -13,7 +14,20 @@ export class HeaderComponent {
 
   }
 
+  isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent) || window.innerWidth < 768;
+  showMenu = false;
+
+  openMenu() {
+    this.showMenu = true;
+  }
+
+  closeMenu() {
+    this.showMenu = false;
+  }
+
   scrollTo(sectionId: string) {
     this.scrollService.scrollTo(sectionId);
+    this.closeMenu();
   }
 }
+
