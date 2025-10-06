28559e5a1c6de237422b9381b0b3b5adf143fb17
Beslim17 <b.montiari@gmail.com>
2025-10-06 14:56:32 +0200

    projects updated

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -14,28 +14,26 @@ export class ProjectsComponent {
     {
       image: "./assets/images/join.png",
       title: "Join",
-      skills: "Angular | TypeScript | HTML | CSS | Firebase",
-      text: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ",
+      skills: "TypeScript | HTML | CSS | Firebase",
+      text: "Plan smarter and get more done with a visual Kanban board designed for simplicity and control. Create tasks, move them through stages, and watch your projects come to life.",
       gitLink: "https://github.com/LeonKoenig17/Join",
-      liveLink: "https://join-440.developerakademie.net/Join/board.html"
+      liveLink: "https://join.leonk-thedev.com"
     },
     {
       image: "./assets/images/pokedex.png",
       title: "Pokedex",
       skills: "JavaScript | HTML | CSS",
-      text: `Explore the full arsenal of the Pokémon card registry in detail. 
-        This application uses data fetched from API's`,
+      text: "Explore the world of Pokémon with this interactive Pokédex powered by the PokéAPI. Search for your favorite Pokémon and discover detailed stats, abilities, types, height, and more.",
       gitLink: "https://github.com/LeonKoenig17/Pokedex",
-      liveLink: "http://leon-koenig.developerakademie.net/Pokedex/index.html"
+      liveLink: "http://pokedex.leonk-thedev.com"
     },
     {
       image: "./assets/images/los-virales.png",
       title: "Los Virales",
       skills: "JavaScript | HTML | CSS",
-      text: `A little post-apocalyptic game that utilizes the principles 
-        of object-orientation.`,
+      text: "Experience a post-apocalyptic zombie survival game powered by object-oriented JavaScript. Defeat the hordes of undead and see if you can beat the game.",
       gitLink: "https://github.com/LeonKoenig17/Los-Virales",
-      liveLink: "https://leon-koenig.developerakademie.net/Los-Virales/index.html"
+      liveLink: "https://los-virales.leonk-thedev.com"
     }
   ]
 }
