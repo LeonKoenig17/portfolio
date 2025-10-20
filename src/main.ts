a4df6050bf6b6845eed5413c578cdb4645dca104
Beslim17 <b.montiari@gmail.com>
2025-10-20 17:44:05 +0200

    correction

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter, withInMemoryScrolling  } from '@angular/router';
 import { routes } from './app/app.routes';
+import { IMAGE_CONFIG } from '@angular/common';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
@@ -11,7 +12,14 @@ bootstrapApplication(AppComponent, {
     provideRouter(routes, withInMemoryScrolling({
       scrollPositionRestoration: 'top',
       anchorScrolling: 'enabled'
-    }))
+    })),
+    {
+      provide: IMAGE_CONFIG,
+      useValue: {
+        disableImageSizeWarning: true,
+        disableImageLazyLoadWarning: true
+      }
+    }
   ]
 })
   .catch((err) => console.error(err));
